033cd2e68bac97907a7adc52b9feaf5873c12d02
Chris Gibb <chris.gibb@outlook.com>
2017-10-11 19:33:43 -0400

    Start using doxygen comments

--- Q1/main.cpp
+++ Q1/main.cpp
@@ -2,6 +2,16 @@
 #include <vector>
 #include <cstdlib>
 
+/**
+ * @brief 
+ * 
+ * Main entry
+ * 
+ * @param argc
+ * @param char*argv[]
+ * 
+ * @return int
+ */
 int main(int argc,char*argv[])
 {
     if(argc <= 1)
@@ -9,7 +19,9 @@ int main(int argc,char*argv[])
         std::cout<<"Insufficient arguments\n";
         return 1;
     }
+
     std::vector<int> nums;
+    
     for(int i = 1; i != argc; ++i)
     {
         nums.push_back(std::atoi(argv[i]));
