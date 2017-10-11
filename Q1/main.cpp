d40ce80fc078b9c2d20df27ac59cebabf32bf276
Chris Gibb <chris.gibb@outlook.com>
2017-10-11 19:26:10 -0400

    initial vscode settings

--- Q1/main.cpp
+++ Q1/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
 int main(int argc,char*argv[])
 {
+    if(argc <= 1)
+    {
+        std::cout<<"Insufficient arguments\n";
+        return 1;
+    }
+    std::vector<float> nums;
+    for(int i = 1; i != argc; ++i)
+    {
+        nums.push_back(std::stof(std::string(argv[i])));
+    }
     return 0;
 }
