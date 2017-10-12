33ec5c01ca15cc68c35471b2079836be099fe1ef
Chris Gibb <chris.gibb@outlook.com>
2017-10-12 16:40:44 -0400

    started question 2

---
+++ Q2/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+#include "../inc/parallelize.hpp"
+
+/**
+ * @brief 
+ * 
+ * Main entry
+ * 
+ * @param argc
+ * @param char*argv[]
+ * 
+ * @return int
+ */
+int main(int argc,char*argv[])
+{
+    if(argc <= 1)
+    {
+        std::cout<<"Insufficient arguments\n";
+        return 1;
+    }
+    else if(argc > 2)
+    {
+        std::cout<<"Too many arguments\n";
+        return 1;
+    }
+
+    return 0;
+}
