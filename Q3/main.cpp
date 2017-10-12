4eac90c90e006172a44516eef066d71088c9189b
Chris Gibb <chris.gibb@outlook.com>
2017-10-12 17:56:23 -0400

    started Q3

---
+++ Q3/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+
+/**
+ * @brief 
+ * 
+ * Main entry
+ * 
+ * @param argc
+ * @param char*argv[]
+ * 
+ * @return int
+ */
+int main(int argc,char*argv[])
+{
+    if(argc <= 1)
+    {
+        std::cout<<"Insufficient arguments\n";
+        return 1;
+    }
+
+    std::vector<int> nums;
+
+    for(int i = 1; i != argc; ++i)
+    {
+        nums.push_back(std::atoi(argv[i]));
+    }
+
+    return 0;
+}
