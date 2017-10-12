008d3e57da82405b84c01fdcd93ec42049d33559
Chris Gibb <chris.gibb@outlook.com>
2017-10-11 22:20:08 -0400

    added comments

--- Q1/inc/average.hpp
+++ Q1/inc/average.hpp
@@ -2,12 +2,30 @@
 #include <future>
 #include <vector>
 
+/**
+ * @brief 
+ * 
+ * Returns the average of all T in nums
+ * 
+ * @param std::vector<T>&nums
+ * 
+ * @return template <class T> T
+ */
 template <class T>
 T getAverage(std::vector<T>&nums)
 {
     return 1;
 }
 
+/**
+ * @brief 
+ * 
+ * Returns a packaged_task representing a parallelized version of getAverage
+ * 
+ * @param 
+ * 
+ * @return std::packaged_task<T(std::vector<T>&)>
+ */
 template <class T>
 std::packaged_task<T(std::vector<T>&)> getAverageParallel()
 {
