1b5c8c3cd290a09ccc9e5b7433accf1bd2ca4936
Chris Gibb <chris.gibb@outlook.com>
2017-10-12 18:15:03 -0400

    refactored

--- Q3/inc/split.hpp
+++ Q3/inc/split.hpp
@@ -5,7 +5,7 @@
  * @brief 
  * 
  * Allocates and fills l with the first half of in and r with the latter half of in.
- * Based on answer by iamantony https://stackoverflow.com/questions/9811235/best-way-to-split-a-vector-into-two-smaller-arrays
+ * Based on answer by Xeo https://stackoverflow.com/questions/9811235/best-way-to-split-a-vector-into-two-smaller-arrays
  * 
  * @param Container*in
  * @param Container*l
@@ -16,10 +16,8 @@
 template <class Container>
 void split(Container*in,Container*l,Container*r)
 {
-    auto middle = in->size()/2;
-    Container::const_iterator middleIt(in->cbegin());
-    std::advance(middleIt,middle);
+    size_t middle = in->size()/2;
 
-    l = new Container(in->begin(),middleIt);
-    r = new Container(middleIt,in->end());
+    l = new Container(in->begin(),in->begin() + middle);
+    r = new Container(in->begin() + middle,in->end());
 }
